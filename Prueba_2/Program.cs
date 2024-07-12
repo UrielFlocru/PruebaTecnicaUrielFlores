f3d66bcc6a7ada59eb4b6f6e82ebf7b2acd6c782
Uriel Antonio Flores <uafc102010@gmail.com>
2024-07-12 14:37:54 -0600

    Prueba tecnica finalizada

---
+++ Prueba_2/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Diagnostics.Contracts;
+using System.IO;
+
+class Program{
+    static string connectionString = "Server=localhost; Database=pruebatecnica; User Id=root; Password=password;";
+
+    static void Main (string[] args){
+        bool continuar=true;
+        while (continuar){
+            Console.WriteLine("Seleccione una opcion:");
+            Console.WriteLine("1. Listar top 10 usuarios");
+            Console.WriteLine("2. Generar archivo CSV");
+            Console.WriteLine("3. Actualizar salario de un usuario");
+            Console.WriteLine("4. Agregar un nuevo usuario");
+            Console.WriteLine("5. Salir");
+            var opcion = Console.ReadLine();
+
+            switch(opcion){
+                case "1":
+                    ListarTop10Usuarios();
+                    break;
+                case "2":
+                    GenerarArchivoCSV();
+                    break;
+                case "3":
+                    Console.WriteLine("Ingrese el ID del usuario:");
+                    int userId = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese el nuevo salario:");
+                    double nuevoSalario = Convert.ToDouble(Console.ReadLine());
+                    ActualizarSalario (userId, nuevoSalario);
+                    break;
+                case "4":
+                    Console.WriteLine("Ingrese el login:");
+                    string login = Console.ReadLine();
+                    Console.WriteLine("Ingrese el nombre:");
+                    string nombre = Console.ReadLine();
+                    Console.WriteLine("Ingrese el apellido paterno:");
+                    string paterno = Console.ReadLine();
+                    Console.WriteLine("Ingrese el apellido materno:");
+                    string materno = Console.ReadLine();
+                    Console.WriteLine("Ingrese el sueldo:");
+                    double sueldo = Convert.ToDouble(Console.ReadLine());
+                    AgregarNuevoUsuario (login, nombre, paterno, materno, sueldo);
+                    break;
+                
+                case "5":
+                    continuar=false;
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida, intente de nuevo");
+                    break;
+
+
+
+            }
+
+        }
+
+    }
+
+
+    static void ListarTop10Usuarios(){
+        using (MySqlConnection connection= new MySqlConnection(connectionString)){
+            string query = "SELECT u.userId, u.Login, u.Nombre, u.Paterno, u.Materno, e.Sueldo, e.FechaIngreso " +
+            "FROM usuarios u JOIN empleados e ON u.userId = e.userId LIMIT 10";
+            MySqlCommand command = new MySqlCommand(query,connection);
+
+            try{
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read()){
+                    Console.WriteLine($"ID: {reader["userId"]}, Login: {reader["Login"]}, Nombre: {reader["Nombre"]} {reader["Paterno"]} {reader["Materno"]}, Sueldo: {reader["Sueldo"]}, Fecha Ingreso: {reader["FechaIngreso"]}");
+                    
+                }
+                reader.Close();
+            }
+
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    static void GenerarArchivoCSV(){
+        using (MySqlConnection connection= new MySqlConnection(connectionString)){
+            string query = "SELECT u.Login, u.Nombre, u.Paterno, u.Materno, e.Sueldo, e.FechaIngreso " +
+                            "FROM usuarios u JOIN empleados e ON u.userId = e.userId";
+
+            MySqlCommand command = new MySqlCommand(query,connection);
+
+            try{
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                using (StreamWriter writer = new StreamWriter("usuarios.csv")){
+                    writer.WriteLine("Login,NombreCompleto,Sueldo,FechaIngreso");
+                    while (reader.Read()){
+                        string nombreCompleto = $"{reader["Nombre"]} {reader["Paterno"]} {reader["Materno"]}";
+                        writer.WriteLine ($"{reader["Login"]},{nombreCompleto}, {reader["Sueldo"]},{reader["FechaIngreso"]}");
+
+                    }
+                }
+                reader.Close();
+            }
+
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    static void ActualizarSalario(int userId, double nuevoSalario){
+        using (MySqlConnection connection= new MySqlConnection(connectionString)){
+            string query = "UPDATE empleados SET Sueldo = @NuevoSueldo WHERE userId = @UserId";
+            
+            MySqlCommand command = new MySqlCommand(query,connection);
+            command.Parameters.AddWithValue("@NuevoSueldo",nuevoSalario);
+            command.Parameters.AddWithValue("@UserId",userId);
+
+            try{
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected>0){
+                    Console.WriteLine("Sueldo actualizado correctamente");
+                }else{
+                    Console.WriteLine("Usuario no encontrado");
+                }
+            }
+
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    static void AgregarNuevoUsuario(string login, string nombre, string paterno, string materno, double sueldo){
+        using (MySqlConnection connection= new MySqlConnection(connectionString)){
+            string queryUsuario = "INSERT INTO usuarios (Login, Nombre, Paterno, Materno) VALUES (@Login, @Nombre, @Paterno, @Materno)";
+            string queryEmpleado = "INSERT INTO empleados (userId, Sueldo, FechaIngreso) VALUES (@UserId, @Sueldo, @FechaIngreso)";
+            
+            MySqlCommand commandUsuario = new MySqlCommand(queryUsuario,connection);
+            commandUsuario.Parameters.AddWithValue("@Login", login);
+            commandUsuario.Parameters.AddWithValue("@Nombre", nombre);
+            commandUsuario.Parameters.AddWithValue("@Paterno", paterno);
+            commandUsuario.Parameters.AddWithValue("@Materno", materno);
+
+            try{
+                connection.Open();
+
+                commandUsuario.ExecuteNonQuery();
+
+                int userId = (int)commandUsuario.LastInsertedId;
+
+                MySqlCommand commandEmpleado = new MySqlCommand(queryEmpleado,connection);
+                commandEmpleado.Parameters.AddWithValue("@UserId",userId);
+                commandEmpleado.Parameters.AddWithValue("@Sueldo",sueldo);
+                commandEmpleado.Parameters.AddWithValue("@FechaIngreso",DateTime.Now);
+
+                commandEmpleado.ExecuteNonQuery();
+
+                Console.WriteLine("Nuevo usuario agreado correctamente");
+
+            }
+
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+
+}
+
--- Prueba_2/README.md
+++ Prueba_2/README.md
@@ -1,1 +1,165 @@
 ## Directorio para la prueba 1 ##
+
+SENTENCIAS:
+
+
+// Crear la base de datos y seleccionarla.
+
+CREATE DATABASE pruebatecnica;
+USE pruebatecnica;
+
+// Crear la tabla usuarios
+
+CREATE TABLE usuarios (
+userId INT(11) NOT NULL AUTO_INCREMENT,
+Login VARCHAR(100) NOT NULL,
+Nombre VARCHAR(100) NOT NULL,
+Paterno VARCHAR(100) NOT NULL,
+Materno VARCHAR(100) NOT NULL,
+PRIMARY KEY (userId)
+);
+
+//Crear la tabla empleados y establecer la relación mediante una llave foranea
+
+CREATE TABLE empleados(
+Sueldo DOUBLE NOT NULL,
+FechaIngreso DATE NOT NULL,
+userId INT(11) NOT NULL,
+FOREIGN KEY (userId) REFERENCES usuarios (userId)
+);
+
+// Insertar los datos a la tabla de usuarios
+
+INSERT INTO usuarios (Login, Nombre, Paterno, Materno) VALUES
+    ('user01', 'BERE', 'NARANJO', 'GONZALEZ'),
+    ('user02', 'ALEXIS', 'CAMPOS', 'NARANJO'),
+    ('user03', 'SERGIO ALEJANDRO', 'CAMPOS', 'HERNANDEZ'),
+    ('user04', 'DIEGO ISMAEL', 'BERUMEN', 'CEDILLO'),
+    ('user05', 'AURORA', 'ESCALANTE', 'PALAFOX'),
+    ('user06', 'JOYCELENE FABIOLA', 'ESTRADA', 'BARBA'),
+    ('user07', 'FRANCISCO', 'ESTRADA', 'GOMEZ'),
+    ('user08', 'LEONARDO DANIEL', 'FARIAS', 'ROSAS'),
+    ('user09', 'RAMON ANDRES', 'FIERROS', 'ROBLES'),
+    ('user10', 'EDGARD ANDRES', 'FLORES', 'OLIVARES'),
+    ('user11', 'MARIA FERNANDA', 'FRANCO', 'ESQUIVEL'),
+    ('user12', 'ALEJANDRO', 'GALVAN', 'MUÑIZ'),
+    ('user13', 'MARTHA ALICIA', 'GUTIERREZ', 'ORTIZ'),
+    ('user14', 'JOSAFAT GERARDO', 'HERNANDEZ', 'SAUCEDO'),
+    ('user15', 'ROSALIA', 'JIMENEZ', 'GONZALEZ'),
+    ('user16', 'LAURA CELENE', 'JIMENEZ', 'RIOS'),
+    ('user17', 'ANGELICA', 'LOPEZ', 'CORTES'),
+    ('user18', 'CRISTIAN IVAN', 'LOPEZ', 'GOMEZ'),
+    ('user19', 'MARLENE GABRIELA', 'LOPEZ', 'MEZA'),
+    ('user20', 'ALEJANDRA', 'MEDINA', 'IBARRA'),
+    ('user21', 'CONSUELO YURIDIANA THALIA', 'MEJIA', 'ALVAREZ'),
+    ('user22', 'JAVIER ADRIAN', 'MEJIA', 'ALVAREZ'),
+    ('user23', 'JUAN CARLOS EVARISTO', 'PEÑA', 'GUTIERREZ'),
+    ('user24', 'JAZMIN ALEJANDRA', 'PEREZ', 'VELEZ'),
+    ('user25', 'GUSTAVO', 'RAMIREZ', 'RIVERA'),
+    ('user26', 'CARLOS NIVARDO', 'RODRIGUEZ', 'ASCENCIO'),
+    ('user27', 'KARLA JOHANA', 'ROMERO', 'LUEVANOS'),
+    ('user28', 'YESSICA YOSELINNE', 'RUIZ', 'HERNANDEZ'),
+    ('user29', 'CHRISTIAN EDUARDO', 'SALAS', 'SANCHEZ'),
+    ('user30', 'LUIS ROBERTO', 'SALDAÑA', 'ESPINOZA'),
+    ('user31', 'ADRIAN', 'SANCHEZ', 'ORTIZ'),
+    ('user32', 'EDUARDO YAIR', 'SUAREZ', 'HERNANDEZ'),
+    ('user33', 'JUAN FRANCISCO', 'TABAREZ', 'GARCIA'),
+    ('user34', 'ZULEICA ELIZABETH', 'TERAN', 'TORRES'),
+    ('user35', 'ADRIANA YUNUHEN', 'VARGAS', 'ALAYA'),
+    ('user36', 'OSCAR URIEL', 'VELAZQUEZ', 'ALVAREZ'),
+    ('user37', 'ERICK DE JESUS', 'CORONA', 'DIAZ'),
+    ('user38', 'MARIA GUADALUPE', 'RAMOS', 'HERNANDEZ'),
+    ('user39', 'JESSICA NOEMI', 'JIMENEZ', 'VENTURA'),
+    ('user40', 'FLOR MARGARITA', 'ROJAS', 'HERNANDEZ'),
+    ('user41', 'LUIS ANTONIO', 'ALVARADO', 'VALENCIA'),
+    ('user42', 'EDGAR IVAN', 'AGUILAR', 'PADILLA'),
+    ('user43', 'LUIS ALFONSO', 'MICHEL', 'SANCHEZ'),
+    ('user44', 'JOSE CARLOS', 'SILVA', 'ROCHA'),
+    ('user45', 'JUDITH', 'RODRIGUEZ', 'REYES'),
+    ('user46', 'BRENDA SORAYA', 'CHAVEZ', 'GARCIA'),
+    ('user47', 'ALMA ROSA', 'MARQUEZ', 'AGUILA');
+
+// Insertar los datos a la tabla de empleados
+INSERT INTO empleados (Sueldo, FechaIngreso, userId) VALUES
+    ('8837','2000-01-11','1'),
+    ('8837','2000-01-11','2'),
+    ('15000','2000-01-11','3'),
+    ('15000','2000-01-11','4'),
+    ('7812','2000-01-18','5'),
+    ('7812','2000-01-18','6'),
+    ('10200','2000-01-18','7'),
+    ('10200','2000-01-18','8'),
+    ('13800','2001-05-20','9'),
+    ('13800','2001-05-20','10'),
+    ('18880','2001-05-20','11'),
+    ('18880','2001-05-20','12'),
+    ('8000','2001-07-13','13'),
+    ('8000','2001-07-13','14'),
+    ('6000','2001-07-13','15'),
+    ('19470','2001-07-13','16'),
+    ('19470','2001-07-13','17'),
+    ('10200','2001-07-16','18'),
+    ('10200','2001-07-16','19'),
+    ('25000','2001-07-16','20'),
+    ('7812','2001-07-16','21'),
+    ('7812','2001-07-16','22'),
+    ('12210','2001-11-24','23'),
+    ('12210','2001-11-24','24'),
+    ('7500','2001-11-24','25'),
+    ('15020','2001-11-24','26'),
+    ('15020','2001-11-24','27'),
+    ('25000','2001-11-24','28'),
+    ('7812','2001-11-24','29'),
+    ('15020','2001-12-12','30'),
+    ('15020','2001-12-12','31'),
+    ('12210','2001-12-12','32'),
+    ('12210','2001-12-12','33'),
+    ('19470','2008-08-17','34'),
+    ('19470','2008-08-17','35'),
+    ('8000','2008-08-17','36'),
+    ('8000','2008-08-17','37'),
+    ('18880','2009-06-11','38'),
+    ('18880','2009-06-11','39'),
+    ('14000','2009-06-11','40'),
+    ('13800','2009-06-11','41'),
+    ('13800','2009-06-11','42'),
+    ('15000','2009-10-06','43'),
+    ('15000','2009-10-06','44'),
+    ('13000','2009-10-06','45'),
+    ('8837','2009-10-06','46');
+
+// Depurar solo los ID diferentes de 6,7,9 y 10 de la tabla usuarios
+
+DELETE FROM usuarios WHERE userId NOT IN (6,7,9,10);
+
+//Actualizar el dato Sueldo en un 10 porciento a los empleados que tienen fechas entre el año 2000 y 2001
+
+ UPDATE empleados SET Sueldo = Sueldo * 1.10
+    WHERE FechaIngreso BETWEEN '2000-01-01' AND '2001-12-31';
+
+//Realiza una consulta para traer el nombre de usuario y fecha de ingreso de los usuarios que gananen mas de 10000 y su apellido comience con T ordernado del mas reciente al mas antiguo
+
+SELECT u.Login, e.FechaIngreso
+    FROM usuarios u
+    JOIN empleados e ON u.userId = e.userId
+    WHERE e.Sueldo > 10000 AND u.Paterno LIKE 'T%'
+    ORDER BY e.FechaIngreso DESC;
+
+//Realiza una consulta donde agrupes a los empleados por sueldo, un grupo con los que ganan menos de 1200 y uno mayor o igual a 1200, ¿cuantos hay en cada grupo?
+
+SELECT
+    CASE
+    WHEN Sueldo < 1200 THEN 'Menos de 1200'
+    ELSE 'Mayor o igual a 1200'
+    END AS SueldoGrupo,
+    COUNT(*) AS cantidad FROM empleados GROUP BY SueldoGrupo;
+
+*** HAY 46 empleados en el grupo 'Mayor o igual a 1200' y 0 en el grupo 'Menos de 1200' ***
+
+
+
+
+
+
+
+
